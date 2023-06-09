553c7016788c0933c7b6feb4ce26c343a46b7571
tpemba100 <tpemba.96@gmail.com>
2023-06-09 13:13:14 -0400

    1st push card

---
+++ script.js
@@ -0,0 +1,65 @@
+let t1, t2, flap;
+
+document.addEventListener("DOMContentLoaded", function () {
+  t1 = gsap.timeline({ paused: true });
+  flap = CSSRulePlugin.getRule(".envelope:before");
+
+  t1.to(flap, {
+    duration: 0.5,
+    cssRule: {
+      rotateX: 180,
+    },
+  })
+    .set(flap, {
+      cssRule: {
+        zIndex: 10,
+      },
+    })
+    .to(".letter", {
+      translateY: -200,
+      duration: 0.9,
+      ease: "back.inOut(1.5)",
+      height: 195,
+    })
+
+    .set(".letter", {
+      zIndex: 40,
+    })
+    .to(".letter", {
+      duration: 0.7,
+      ease: "back.out(.4)",
+      translateY: -5,
+      translateZ: 250,
+      rotate: -90,
+    })
+    .to(".d_link", {
+      duration: 0.3,
+      ease: "back.out(.4)",
+      rotate: 90,
+      translateX: -170,
+      fontSize: 13,
+    })
+    .to(".d_link", {
+      opacity: 1,
+    });
+
+  t2 = gsap.timeline({ paused: true });
+  t2.to(".shadow", {
+    delay: 1.4,
+    width: 450,
+    boxShadow: "-75px 150px 10px 5px #eeeef3",
+    ease: "back.out(.2)",
+    duration: 0.7,
+    display: none,
+  });
+});
+
+function openCard() {
+  t1.play();
+  t2.play();
+}
+
+function closeCard() {
+  t1.reverse();
+  t2.reverse();
+}
